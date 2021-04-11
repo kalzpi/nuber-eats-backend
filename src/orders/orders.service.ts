8c6aff9f57e3c8571802659be91cc5f1caa5f87c
kalzpi <kalzpi@gmail.com>
2021-04-12 00:31:46 +0900

    22.6 Making Order part Six

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -93,7 +93,7 @@ export class OrderService {
       await this.pubsub.publish(NEW_PENDING_ORDER, {
         pendingOrders: { order, ownerId: restaurant.ownerId },
       });
-      return { ok: true };
+      return { ok: true, orderId: order.id };
     } catch (error) {
       return { ok: false, error: 'Cannot create order' };
     }
